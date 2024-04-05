bc5d4a3abe713c643f92dac4d610488f0fe14be4
vaquero <froilan@virtual-artifact.com>
2024-04-05 10:54:05 +0100

    feature: add api calls to hg moderator api

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue'
+import { client } from "@gradio/client";
 
 defineProps({
   msg: String,
@@ -10,8 +11,20 @@ const count = ref(0)
 onMounted(async () => {
   const response = await fetch('/.netlify/functions/hello-world').then(response => response.text())
   const pokedex = await fetch('/.netlify/functions/pokedex').then(response => response.json())
-  console.log(response)
-  console.log(pokedex)
+  //console.log(response)
+  //console.log(pokedex)
+
+  const censorMe = await client("https://duchaba-friendly-text-moderation.hf.space/--replicas/dg2zs/");
+  const resultA = await censorMe.predict("/censor_me", [
+    "Hello!!", // string  in 'Enter Text:' Textbox component		
+		0.005, // number (numeric value between 0.005 and 0.1) in 'Personalize Safer Value: (larger value is less safe)' Slider component
+  ]);
+  console.log(resultA.data);
+
+  const toxic = await client("https://duchaba-friendly-text-moderation.hf.space/--replicas/dg2zs/");
+  const resultB = await toxic.predict("/fetch_toxic_tweets", [  ]);
+
+  console.log(resultB.data);
 })
 
 </script>
