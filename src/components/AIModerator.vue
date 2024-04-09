30f6e1c3be5003c628e33aed362d21d1df92c5b2
vaquero <froilan@virtual-artifact.com>
2024-04-09 16:05:46 +0100

    chores: clean up code

--- src/components/AIModerator.vue
+++ src/components/AIModerator.vue
@@ -9,29 +9,13 @@
   import { ref } from 'vue'
   import axios from 'axios';
   
-  //const AI_ANALYZER = '/.netlify/functions/post-analyzer'
   const AI_ANALYZER = 'https://g0xxitfr9a.execute-api.us-east-1.amazonaws.com/analyze'
   const inputText = ref('')
   const prediction = ref('')
 
   async function onSubmit() {
     if (inputText.value === '') return
-
-    console.log('clicked!', inputText.value)
-
-    const payload = {
-      "text": inputText.value
-    }
-    
-    /*
-    const response = await fetch(AI_ANALYZER, {
-      method: "POST", // or 'PUT'
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    });
-    */
+    const payload = { "text": inputText.value }
     axios.post(AI_ANALYZER, JSON.stringify(payload), {
       headers: {
         'Content-Type': 'application/json',
