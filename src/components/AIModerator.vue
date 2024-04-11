86256a28635b33e8dea14cee025eb9a124bab6af
vaquero <froilan@virtual-artifact.com>
2024-04-12 01:22:59 +0200

    feature: add sober post to list and display, show snackbar on toxic post

--- src/components/AIModerator.vue
+++ src/components/AIModerator.vue
@@ -1,8 +1,37 @@
 <template>
   <h1>A.I. Moderator</h1>
+  <p>I'd love to know what you think about anything. Feel free to express yourself, but don't be toxic. Nobody loves toxicity :( But if you do, I'm sure there is some corner of the internet that will welcome you. </p>
   <v-textarea v-model="inputText" label="Enter text here"></v-textarea>
   <v-btn rounded="lg" size="x-large" block @click=onSubmit>Submit</v-btn>
   <div>{{ prediction }}</div>
+  <h2 class="mt-4">Previous Post</h2>
+  <div>
+    <v-virtual-scroll
+      :height="300"
+      :items="acceptedPosts"
+    >
+      <template v-slot:default="{ item }">
+        {{ item }}
+      </template>
+    </v-virtual-scroll>
+  </div>
+<div class="text-center ma-2">
+    <v-snackbar
+      v-model="snackbar"
+    >
+      {{ toxicText }}
+
+      <template v-slot:actions>
+        <v-btn
+          color="pink"
+          variant="text"
+          @click="snackbar = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
 </template>
 
 <script setup>
@@ -12,6 +41,9 @@
   const AI_ANALYZER = 'https://g0xxitfr9a.execute-api.us-east-1.amazonaws.com/analyze'
   const inputText = ref('')
   const prediction = ref('')
+  const acceptedPosts = ref([''])
+  const snackbar = ref(false)
+  const toxicText = ref('')
 
   async function onSubmit() {
     if (inputText.value === '') return
@@ -23,11 +55,23 @@
     })
     .then(function (response) {
       console.log(response);
-      prediction.value = response.data.result
-    })
+      processResponse(response.data.result, inputText.value) })
     .catch(function (error) {
       console.log(error);
     });
+  }
+
+  function processResponse(res, post) {
+    if(res.includes("sober")){
+      //post response
+      acceptedPosts.value.unshift(post)
+    } else {
+      console.log(typeof res)
+      toxicText.value = 'Your post was found to be toxic, and will not be posted'
+      snackbar.value = true
+      // alert toxicity
+      
+    }
   }
 
 </script>
